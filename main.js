a5a576fd9242d3f0af0158bbb9a91e3d397bbbb2
Ormatist <thescientist771.mail@gmail.com>
2022-09-30 16:37:50 +0100

    full func
    full functionality for all events and multipliers (where nessesary) working on overall results and then data stroage

--- main.js
+++ main.js
@@ -16,25 +16,10 @@ function pointAward(paLocation) {
     var points = document.getElementById(paLocation.replace('PA', 'FP')).value;
     document.getElementById(paLocation).innerHTML = points;
     var eventType = paLocation[2].concat(paLocation[3]);
-    switch(eventType) {
-        case "BR":
-            var multiplier = 1
-            break;
-        case "AR":
-            var multiplier = 2
-            break;
-        case "SR":
-            var multiplier = 2
-            break;
-        case "TR":
-            var multiplier = 2
-            break;
-        case "SR":
-            var multiplier = 2
-            break;
-        case "BF":
-            var multiplier = 2
-            break;
+    if (eventType == "AB") {
+        var multiplier = 2
+    } else {
+        var multiplier = 1
     }
     var multiplied = parseInt(points) * multiplier;
     document.getElementById(paLocation.replace('PA', 'MU')).innerHTML = multiplied;
