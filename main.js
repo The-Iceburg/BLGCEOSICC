8cdfe2ccaf70eab43f5dd9f4e2a0681d388f10d7
Dockuin <joshuanicholson007@gmail.com>
2022-10-19 00:04:51 +0100

    Complete Function...
    Functions completely as intended - with team ordering
    However, does lack the ability to save between session (Maybe adding?)

--- main.js
+++ main.js
@@ -25,22 +25,51 @@ function resultsGen() {
 
     var results = {};
     const events = ["BR", "AB", "SR", "TR", "RR", "BF"];
-
+    const orderName = ["1teamname","2teamname","3teamname","4teamname","5teamname","6teamname","7teamname","8teamname"]
+    const orderPoints = ["1teampoints","2teampoints","3teampoints","4teampoints","5teampoints","6teampoints","7teampoints","8teampoints"]
+    const teams = ["team1","team2","team3","team4","team5","team6","team7","team8"]
+    const pointsTotals = []
+    const indexOrder = []
+    const names = []
     for (let k = 0; k < 8; k++) {
-
+    
         var pointotal = 0;
 
+        
         for (let i = 0; i < 6; i++) {
 
             var id = "R".concat(k + 1).concat(events[i]).concat("MU");
             var pointsAwarded = document.getElementById(id).textContent;
             pointotal += parseInt(pointsAwarded);
         }
-        results["team".concat(k + 1).concat("name")] = pointotal;
-    }
+        pointsTotals.push(pointotal)
+        results["team".concat(k + 1)] = pointotal;
 
-    document.getElementById("1teamname").innerHTML = document.getElementById("team1").value;
-    document.getElementById("1teampoints").innerHTML = results["team1name"]
+        
+    }
+    var pTcopy = pointsTotals.slice()    
+    pTcopy.sort(function(a, b){return b - a})
+
+    console.log(pointsTotals)
+    for (let i = 0; i < 8; i++) {
+        var indexes = pointsTotals.indexOf(pTcopy[i], i)
+        if (indexes == -1) {
+            indexes = pointsTotals.indexOf(pTcopy[i], 0)
+        }
+
+        indexOrder.push(indexes)
+    }
+    
+    for (let i = 0; i < 8; i++) {
+        names.push(document.getElementById(teams[i]).value)
+    }
+    
+    for (let k = 0; k < 8; k++) {
+        var nameOrder = indexOrder[k] 
+        document.getElementById(orderName[k]).innerHTML = names[nameOrder];
+        document.getElementById(orderPoints[k]).innerHTML = pTcopy[k]
+        
+    }
 
 
 
